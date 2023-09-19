e10c9ee1802a42ec043c05be9c68b77d620b9014
khairiyanhidayat.ramadhan17@gmail.com <khairiyanhidayat.ramadhan17@gmail.com>
2023-09-19 16:14:23 +0700

    belajar membuat hello world

---
+++ java/belajar-1/test-env/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!, dan kita sudah menamatkan JAVA");
+        int a = 4;
+        System.out.println(a);
+    }
+}
+
